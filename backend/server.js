389fd6600a386cf8f9c53046efcfdabec80a3511
Krishna Sikheriya <krishnasikheriya001@gmail.com>
2025-07-17 01:26:12 +0530

    improved server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,3 @@
-delete process.env.HTTP_PROXY;
-delete process.env.http_proxy;
-delete process.env.HTTPS_PROXY;
-delete process.env.https_proxy;
-
-process.env.NO_PROXY = 'upload.imagekit.io';
-
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
